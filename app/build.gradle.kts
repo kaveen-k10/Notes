57f9d2d175cddbe50cbd094048e707b9a351c828
kaveen-k10 <it22235992@my.sliit.lk>
2024-05-12 23:44:20 +0530

    Adapter implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -26,6 +27,10 @@ android {
             )
         }
     }
+
+    buildFeatures{viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,4 +49,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Room dependencies
+    val room_version = "2.6.1"
+
+    implementation ("androidx.room:room-ktx:$room_version")
+    implementation ("androidx.room:room-runtime:$room_version")
+    kapt ("androidx.room:room-compiler:$room_version")
+
+    // Lifecycle dependencies
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+
+    // Coroutines dependency
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
 }
